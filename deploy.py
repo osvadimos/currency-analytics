56e938eab45846f092605953e7c234b54fc25b97
Vadim Osmolovski <osvadimos@gmail.com>
2020-12-20 11:05:38 +0300

    Start adding deploy process

---
+++ deploy.py
@@ -0,0 +1,12 @@
+import logging
+import os
+from logging.config import fileConfig
+
+from data_storage.data.DeployService import DeployService
+
+fileConfig(os.path.dirname(os.path.realpath(__file__)) + '/logging_config.ini')
+logger = logging.getLogger()
+
+if __name__ == '__main__':
+    logging.info("Start deploying")
+    market_manager = DeployService()
